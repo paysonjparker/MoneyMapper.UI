8d9733233014621e0bbbf8ccf544a1d105e2a9cf
Payson Parker <paysonparker@costco.com>
2024-10-23 09:35:16 -0700

    Added all services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/core/environments/environments.ts
@@ -0,0 +1,3 @@
+export const environments = {
+    moneyMapperLocalApi: 'https://localhost:7187/api'
+}
