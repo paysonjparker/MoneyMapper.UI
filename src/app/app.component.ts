2b7209129486381b328521cb884e0a622f25e471
Payson Parker <paysonparker@costco.com>
2024-10-23 10:29:53 -0700

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PrimeNGConfig } from 'primeng/api';
 import { NavigationBarComponent } from './core/components/navigation-bar/navigation-bar.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     FormsModule,
     ReactiveFormsModule,
@@ -16,6 +19,20 @@ import { NavigationBarComponent } from './core/components/navigation-bar/navigat
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'MoneyMapper.UI';
+export class AppComponent implements OnInit {
+  title = 'Money Mapper';
+
+  /**
+   * Constructor for App Component.
+   * @param primeNgConfig - An injected instance of PrimeNg's {@link PrimeNGConfig}
+   */
+  constructor(private primeNgConfig: PrimeNGConfig) { }
+
+  /**
+   * NgOnInit lifecycle hook. Executes when application is initialized.
+   */
+  ngOnInit() {
+    // Set PrimeNg Input style to 'filled'.
+    this.primeNgConfig.inputStyle = signal<"outlined" | "filled">("filled")
+  }
 }
--- src/app/core/components/navigation-bar/navigation-bar.component.ts
+++ src/app/core/components/navigation-bar/navigation-bar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    AvatarModule
+  ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss'
 })
-export class NavigationBarComponent {
+export class NavigationBarComponent implements OnInit {
 
+  menuItems!: MenuItem[]
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home'
+      },
+    ];
+  }
 }
